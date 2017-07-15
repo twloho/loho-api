8c4d5ae7afb120b41aa258fc451c2c87ecb79745
LYB <king800116@gmail.com>
2017-07-15 16:32:18 +0800

    add create members table

---
+++ app/Member/Member.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Member;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type',
+        'serial_number',
+        'last_name',
+        'first_name',
+        'gender',
+        'identification',
+        'birthday',
+        'email',
+        'home_phone_number',
+        'cell_phone_number',
+        'postcode',
+        'city',
+        'zone',
+        'address',
+        'contact_last_name',
+        'contact_first_name',
+        'contact_cell_phone_number',
+    ];
+}
---
+++ database/migrations/2017_07_15_081533_create_members_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('type');
+            $table->string('serial_number');
+
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->tinyInteger('gender');
+            $table->string('identification');
+            $table->dateTimeTz('birthday');
+
+            $table->string('email');
+            $table->string('home_phone_number');
+            $table->string('cell_phone_number');
+
+            $table->string('postcode');
+            $table->string('city');
+            $table->string('zone');
+            $table->string('address');
+
+            $table->string('contact_last_name');
+            $table->string('contact_first_name');
+            $table->string('contact_cell_phone_number');
+
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('members');
+    }
+}
