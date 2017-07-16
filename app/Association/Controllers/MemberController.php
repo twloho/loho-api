01ac523544d98f1458a1c904519d51011dbe5922
LYB <king800116@gmail.com>
2017-07-16 09:36:09 +0800

    add member repository

--- app/Association/Controllers/MemberController.php
+++ app/Association/Controllers/MemberController.php
@@ -2,12 +2,19 @@
 
 namespace App\Association\Controllers;
 
+use App\Association\Repositories\MemberRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Association\Member;
 
 class MemberController extends Controller
 {
+    protected $memberRepository;
+
+    public function __construct(MemberRepository $memberRepository)
+    {
+        $this->memberRepository = $memberRepository;
+    }
+
     /**
      * @SWG\Get(
      *   path="/members",
@@ -34,35 +41,9 @@ class MemberController extends Controller
      *   )
      * )
      */
-
-    /**
-     * @SWG\Definition(
-     *   definition="Member",
-     *   type="object",
-     *   required="['id']",
-     *   @SWG\Property(property="id", type="integer"),
-     *   @SWG\Property(property="type", type="integer"),
-     *   @SWG\Property(property="serial_number", type="string"),
-     *   @SWG\Property(property="last_name", type="string"),
-     *   @SWG\Property(property="first_name", type="string"),
-     *   @SWG\Property(property="gender", type="integer"),
-     *   @SWG\Property(property="identification", type="string"),
-     *   @SWG\Property(property="birthday", type="string", format="date"),
-     *   @SWG\Property(property="email", type="string"),
-     *   @SWG\Property(property="home_phone_number", type="string"),
-     *   @SWG\Property(property="cell_phone_number", type="string"),
-     *   @SWG\Property(property="postcode", type="string"),
-     *   @SWG\Property(property="city", type="string"),
-     *   @SWG\Property(property="zone", type="string"),
-     *   @SWG\Property(property="address", type="string"),
-     *   @SWG\Property(property="contact_last_name", type="string"),
-     *   @SWG\Property(property="contact_first_name", type="string"),
-     *   @SWG\Property(property="contact_cell_phone_number", type="string")
-     * )
-     */
     public function index()
     {
-        $members = Member::all();
+        $members = $this->memberRepository->getAll();
         $data = [
             'meta' => [
                 'status' => 200,
